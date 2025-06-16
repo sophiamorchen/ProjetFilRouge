cc55be5046064b7955da7ff43de68b7610473284
Sophia M <sophia.moerchen@gmail.com>
2025-06-16 18:07:43 +0200

    Ajout page editPAssword et modif page account

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/carte", "La carte", "/pages/carte.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
   //  new Route("/indexfront.html", "Accueil", "/pages/home.html"), solution si route ne marche pas. voir forum.
 ]
 
